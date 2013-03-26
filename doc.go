1f89aa80c3d69d6c81bbcf5b04ea097fdeed8275
Song Gao <song@gao.io>
2013-03-26 08:42:37 -0500

    doc.go

---
+++ doc.go
@@ -0,0 +1,2 @@
+// Package water is a simple TUN/TAP library. It efficiently works with standard packages like io, bufio, etc..
+package water
---
+++ waterutil/doc.go
@@ -0,0 +1,2 @@
+// Package waterutil provides utility functions for interpreting MAC farme headers and IP packet headers. It defines some constants such as protocol numbers and ethernet frame types. Use waterutil along with package water to work with TUN/TAP interface data.
+package waterutil
