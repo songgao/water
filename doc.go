1c41478c900b7388e3e0f0637efd2ced975dada4
Song Gao <song@gao.io>
2013-03-26 08:47:37 -0500

    doc.go

--- doc.go
+++ doc.go
@@ -1,2 +1,2 @@
-// Package water is a simple TUN/TAP interface library that efficiently works with standard packages like io, bufio, etc..
+// Package water is a simple TUN/TAP interface library that efficiently works with standard packages like io, bufio, etc.. Use waterutil with it to work with TUN/TAP packets/frames.
 package water
