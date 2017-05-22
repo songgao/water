c59a2fb87d39ed930f880a38955ddbf5212ec1a6
Matthew Ellison <matt@arroyonetworks.com>
2017-05-22 16:22:27 -0400

    Removes Redundant Type Conversion

--- syscalls_linux.go
+++ syscalls_linux.go
@@ -22,7 +22,7 @@ type ifReq struct {
 }
 
 func ioctl(fd uintptr, request int, argp uintptr) error {
-	_, _, errno := syscall.Syscall(syscall.SYS_IOCTL, uintptr(fd), uintptr(request), argp)
+	_, _, errno := syscall.Syscall(syscall.SYS_IOCTL, fd, uintptr(request), argp)
 	if errno != 0 {
 		return os.NewSyscallError("ioctl", errno)
 	}
